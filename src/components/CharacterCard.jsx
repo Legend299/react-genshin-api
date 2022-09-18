85306f904f2f46ef3d96c8771b596edf1130edab
Francisco Antonio <mejiadominguez00@gmail.com>
2022-09-18 15:50:12 -0700

    update: loading component

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,70 +1,45 @@
 import { useContext } from 'react';
 import { CharacterContext } from '../context/CharacterContext';
+import Loading from './Loading';
 
 function CharacterCard() {
   const { character, character_image, loading } = useContext(CharacterContext);
 
   console.log(character);
 
-  if (!loading)
-    return (
-      <div className="flex h-screen">
-        <div className="m-auto">
-          <div role="status">
-            <svg
-              className="inline mr-4 w-32 h-32 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-          </div>
-        </div>
-      </div>
-    );
+  if (!loading) return <Loading />;
 
   return (
-    <div class="p-6">
-      <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+    <div className="p-6">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
         {character.map((item, index) => (
-          <div class="overflow-hidden rounded-2xl bg-gray-50">
-            {/* class="flex items-center h-[180px] overflow-hidden" */}
-
+          <div className="overflow-hidden rounded-2xl bg-gray-50">
             <img
-              // className='object-scale-down h-42 w-52 mx-auto'
               className="object-top w-full bg-slate-800"
               src={character_image[index]}
               alt={item.name + '_gacha_image'}
             />
 
-            <div class="p-6">
-              <div class="flex flex-col items-start justify-between sm:flex-row sm:items-center">
+            <div className="p-6">
+              <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
                 <div>
-                  <p class="text-gray-400 mb-4">
+                  <p className="text-gray-400 mb-4">
                     {item.affiliation} • {item.nation}
                   </p>
-                  <h2 class="mt-2 text-lg font-semibold bg-slate-700 rounded-xl inline text-white p-2">
+                  <h2 className="mt-2 text-lg font-semibold bg-slate-700 rounded-xl inline text-white p-2">
                     {item.name}
                   </h2>
                 </div>
-                <span class="mt-2 inline-block rounded-full p-3 text-lg font-medium text-black">
+                <span className="mt-2 inline-block rounded-full p-3 text-lg font-medium text-black">
                   {item.vision}
                 </span>
               </div>
 
-              <hr class="mt-4 mb-4" />
+              <hr className="mt-4 mb-4" />
 
-              <div class="flex flex-wrap justify-between">
-                <p class="inline-flex items-center">
-                  <span class="ml-2 text-gray-600">
+              <div className="flex flex-wrap justify-between">
+                <p className="inline-flex items-center">
+                  <span className="ml-2 text-gray-600">
                     {item.description
                       ? item.description
                       : 'No se encontró una descripción'}
